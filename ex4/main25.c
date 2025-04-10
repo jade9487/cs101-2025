163368a3c451e317793ffdde5f88613adc294a85
jade9487 <jade.hsiao95@gmail.com>
2025-04-10 15:36:04 +0800

    Update main25.c

--- ex4/main25.c
+++ ex4/main25.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
 
-int multi(int i, int j){
-    
-    if(j==1 && i==1){
-        printf("%d*%d = %d ",i,j,i*j);
-        return 0;
-    }
-    else if(j!=0){
-        multi(i,j-1);
-        printf("%d*%d = %d ",i,j,i*j);
-    }
-   
-    
-    else {
-        j=9;
-        
-        multi(i-1,j);
-    }
-    
-    if (j==9){
-        printf("\n");
-    }
+int my_strlen(char* s){
+    if(!*s) return 0;
+    return 1 + my_strlen(s+1);
     
 }
 
-int main()
-{
-    int i=9,j=9;
+int main(){
     
-    multi(i,j);
-
+    char s[] = "IU is a girl!";
+    
+    printf("(%s) len = %d\n",s,my_strlen(s));
+    
+    
     return 0;
 }
+
 
